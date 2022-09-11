49d38af26552bc88daae133e59607def3596d6fb
prok3z <43358190+prok3z@users.noreply.github.com>
2022-09-10 21:15:28 -0300

    Update sub.sh

--- sub.sh
+++ sub.sh
@@ -13,33 +13,33 @@ domain=$1
 
 logo(){
 
-echo -e "${Cyan} __.   .    .__  Automation for SubDomains                        
-(__ . .|_   |  \* __ _. _ .  , _ ._.  .
-.__)(_|[_)  |__/|_) (_.(_) \/ (/,[  \_|
-                           @prok3z  ._|
-	./sub.sh target.com		   
-			"
+        echo -e "${Cyan} __.   .    .__  Automation for SubDomains                        
+        (__ . .|_   |  \* __ _. _ .  , _ ._.  .
+        .__)(_|[_)  |__/|_) (_.(_) \/ (/,[  \_|
+                                           @prok3z  ._|
+                                                ./sub.sh target.com                
+                                "
 
 
-}
-passive_enum(){
-echo "Scanner Initialized"
-sleep 1
-subfinder -silent -d $domain -o subfin
-assetfinder -subs-only $domain | tee asset
-amass enum -passive -d $domain -o passive.txt
-findomain -t $domain -q -u find
-cat subfin asset passive.txt find > urls
-cat urls | anew hosts
-cat hosts | httpx -silent -timeout 50 -mc 200,301,302,403 -o domains
-rm -rf subfin asset passive.txt find hosts urls
-echo "Successful Enumeration"
-}
+                        }
+                passive_enum(){
+                        echo "Scanner Initialized"
+                        sleep 1
+                        subfinder -silent -d $domain -o subfin
+                        assetfinder -subs-only $domain | tee asset
+                        amass enum -passive -d $domain -o passive.txt
+                        findomain -t $domain -q -u find
+                        cat subfin asset passive.txt find > urls
+                        cat urls | anew hosts
+                        cat hosts | httpx -silent -timeout 50 -mc 200,301,302,403 -o domains
+                        cat domains >> url
+                        rm -rf subfin asset passive.txt find hosts urls domains
+                        echo "Successful Enumeration"
+                }
 
-if [ "$1" ];then
-	passive_enum
-elif [ "$0" ];then
-	logo	
-fi
-
+        if [ "$1" ];then
+                        passive_enum
+                elif [ "$0" ];then
+                                logo
+        fi
 
